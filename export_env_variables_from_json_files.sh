e837723e1b012918f822e4ff0415218ceea1fef9
yann zerlaut <yann.zerlaut@cnrs.fr>
2019-08-08 20:55:26 +0200

    adding the workflow to run the procedure

--- export_env_variables_from_json_files.sh
+++ export_env_variables_from_json_files.sh
@@ -1,22 +1,25 @@
-#! /bin/bash
+# ! /bin/bash
 
 output=$(python -c '
 import json, os
 file1 = os.path.join(os.path.expanduser("~"), "Downloads", "HBP.json")
 if os.path.isfile(file1):
    data1=json.load(open(file1))
+   # print("echo the new HBP token is ["+str(data1["access_token"][:20])+"..."+str(data1["access_token"][-20:])+"]")
 else:
    print("echo ** /!\ ", file1, "not found **")
 file2 = os.path.join(os.path.expanduser("~"), "Downloads", "config.json")
 if os.path.isfile(file2):
    data2=json.load(open(file2))
+   # print("echo the new CSCS token is ["+str(data2["auth"]["token"]["access_token"][:20])+"..."+str(data2["auth"]["token"]["access_token"][-20:])+"]")
 else:
    print("echo ** /!\ ", file2, "not found **")
 
 magic=""
-magic+="export HBP_token=\""+str(data1["access_token"])+"\"\n";
-magic+="export HBP_STORAGE_TOKEN=\""+str(data2["auth"]["token"]["access_token"])+"\"" ; 
+magic+="export HBP_token="+str(data1["access_token"])+"\n";
+magic+="export HBP_STORAGE_TOKEN="+str(data2["auth"]["token"]["access_token"])+"" ; 
 print(magic)')
 
 eval "$output"
+echo $output
 
